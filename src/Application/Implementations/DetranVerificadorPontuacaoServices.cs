467b1a475f371a838e6e9a6053f3b622472de061
ruandias <ruan_ndias@hotmail.com>
2020-12-30 00:01:28 -0300

    adicionado serviço de pontuação

---
+++ src/Application/Implementations/DetranVerificadorPontuacaoServices.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DesignPatternSamples.Application.DTO;
+using DesignPatternSamples.Application.Repository;
+using DesignPatternSamples.Application.Services;
+
+namespace DesignPatternSamples.Application.Implementations
+{
+    public class DetranVerificadorPontuacaoServices : IDetranVerificadorPontuacaoService
+    {
+        private readonly IDetranVerificadorPontuacaoFactory _Factory;
+
+        public DetranVerificadorPontuacaoServices(IDetranVerificadorPontuacaoFactory factory)
+        {
+            _Factory = factory;
+        }
+
+        public Task<IEnumerable<Pontuacao>> ConsultarPontuacao(Condutor condutor)
+        {
+            var repository = _Factory.Create(condutor.UF);
+            return repository.ConsultarPontuacao(condutor);
+        }
+    }
+}
